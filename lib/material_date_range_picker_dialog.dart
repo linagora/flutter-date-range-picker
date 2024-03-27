b50904dd787027650192177ce1f6123829d15c36
DatDang <tddang@linagora.com>
2024-03-27 17:23:55 +0700

    Fix background color of single view date picker

--- lib/material_date_range_picker_dialog.dart
+++ lib/material_date_range_picker_dialog.dart
@@ -77,35 +77,49 @@ class MaterialDateRangePickerDialog {
       SelectDateActionCallback? selectDateActionCallback
     }
   ) {
-    showModalBottomSheet(
+    showGeneralDialog(
       context: context,
-      isScrollControlled: true,
       barrierColor: Colors.black26,
-      backgroundColor: Colors.transparent,
-      enableDrag: false,
-      constraints: ResponsiveUtils.isLandscapeMobile(context)
-        ? const BoxConstraints(maxWidth: 450)
-        : null,
-      shape: RoundedRectangleBorder(
-        borderRadius: ResponsiveUtils.isMobile(context)
-          ? BorderRadius.only(
-              topLeft: Radius.circular(radius ?? 16.0),
-              topRight: Radius.circular(radius ?? 16.0)
-            )
-          : BorderRadius.all(Radius.circular(radius ?? 16.0))
-      ),
-      builder: (context) {
-        return PointerInterceptor(child: Padding(
-          padding: MediaQuery.of(context).viewInsets,
-          child: SingleViewDatePicker(
-            title: title,
-            radius: radius,
-            autoClose: autoClose,
-            currentDate: currentDate,
-            selectDateActionCallback: selectDateActionCallback
+      useRootNavigator: false,
+      pageBuilder: (context, animation, _) {
+        return AnimatedBuilder(
+          animation: animation,
+          child: Container(
+            alignment: ResponsiveUtils.isMobile(context)
+              ? Alignment.bottomCenter
+              : Alignment.center,
+            constraints: ResponsiveUtils.isLandscapeMobile(context)
+              ? const BoxConstraints(maxWidth: 450)
+              : null,
+            child: Material(
+              borderRadius: ResponsiveUtils.isMobile(context)
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(radius ?? 16.0),
+                    topRight: Radius.circular(radius ?? 16.0))
+                : BorderRadius.all(Radius.circular(radius ?? 16.0)),
+              type: MaterialType.transparency,
+              child: PointerInterceptor(child: Padding(
+                padding: MediaQuery.viewInsetsOf(context),
+                child: SingleViewDatePicker(
+                  title: title,
+                  radius: radius,
+                  autoClose: autoClose,
+                  currentDate: currentDate,
+                  selectDateActionCallback: selectDateActionCallback
+                ),
+              )),
+            ),
           ),
-        ));
-      }
+          builder: (BuildContext context, Widget? child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(0, 1),
+                end: Offset.zero)
+                  .animate(animation),
+              child: child);
+          },
+        );
+      },
     );
   }
 }
